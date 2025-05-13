652848f02e07a3d7344794345b5c9dcadf72a883
Bruno H. G. Gouvea <brunohenriquebh@gmail.com>
2025-05-13 17:30:44 -0300

    Allow external net.server

--- index.ts
+++ index.ts
@@ -1,82 +1,91 @@
-/**
- *  Asterisk AGI (FastAGI) Server for Nodejs
- *  @module node-asteriskagi
- *  @license MIT
- *  @author Corey S. McFadden <cmcfadden@clearlyip.com>
- *  @copyright This project is not affiliated with, endorsed by, or sponsored by Digium Inc. or Sangoma Technologies Corp, holders of the "Asterisk" trademark, which is used here for identification purposes only.
- */
-
 import * as net from "net";
 import events from "events";
 import { AGIChannel } from "./channel";
 
 class AGIServer extends events.EventEmitter {
   public port: number = 4573;
-  private fAGI!: net.Server;
-  constructor(props: { port?: number }) {
+  private fAGI?: net.Server;
+
+  constructor(props: { port?: number; server?: net.Server }) {
     super();
     try {
       this.port = props?.port || this.port;
-      this._bind();
+
+      if (props.server) {
+        // Usa servidor TCP externo (ex: criado no cluster)
+        this.fAGI = props.server;
+        this.fAGI.on("connection", this._handleSocket.bind(this));
+        this.emit("ready", "external");
+      } else {
+        // Cria servidor interno como antes
+        this._bind();
+      }
     } catch (err) {
       console.log(`AGIServer error`, err);
     }
   }
 
   /**
-   * Bind ports+listeners and signal ready.
+   * Cria e escuta o servidor, se não for externo
    */
   _bind() {
     try {
-      this.fAGI = net.createServer((socket) => {
-        const remoteServer: string | false = socket?.remoteAddress
-          ? socket.remoteAddress.split(":").pop() || false
-          : false;
-
-        let dataBuffer = "";
-        socket.on("data", async (data) => {
-          dataBuffer += data.toString();
-          if (dataBuffer.includes("\n\n")) {
-            socket.removeAllListeners("data");
-            const agiVariables = dataBuffer
-              .split("\n")
-              .reduce<{ [key: string]: string }>((acc, line) => {
-                if (line.startsWith("agi_")) {
-                  const [key, value] = line
-                    .split(":")
-                    .map((item) => item.trim());
-                  acc[key.substring(4)] = value;
-                }
-                return acc;
-              }, {});
-
-            // Hangup detection
-            socket.on("data", async (data) => {
-              if (data.toString().includes("HANGUP")) {
-                socket.removeAllListeners("data");
-                socket.end();
-              }
-            });
-
-            const call = new AGIChannel({
-              ...agiVariables,
-              remoteServer,
-              socket,
-            });
-            this.emit("call", call);
-
-            socket.on("end", () => {
-              call.emit("hangup");
-            });
-          }
-        });
-      });
+      this.fAGI = net.createServer(this._handleSocket.bind(this));
       this.fAGI.listen(this.port, () => {
         this.emit("ready", this.port);
       });
     } catch (err) {
-      console.log(`AGIServer`, err);
+      console.log(`AGIServer bind error`, err);
     }
   }
+
+  /**
+   * Lida com cada conexão AGI
+   */
+  _handleSocket(socket: net.Socket) {
+    const remoteServer: string | false = socket?.remoteAddress
+      ? socket.remoteAddress.split(":").pop() || false
+      : false;
+
+    let dataBuffer = "";
+    socket.on("data", async (data) => {
+      dataBuffer += data.toString();
+      if (dataBuffer.includes("\n\n")) {
+        socket.removeAllListeners("data");
+
+        const agiVariables = dataBuffer
+          .split("\n")
+          .reduce<{ [key: string]: string }>((acc, line) => {
+            if (line.startsWith("agi_")) {
+              const [key, value] = line
+                .split(":")
+                .map((item) => item.trim());
+              acc[key.substring(4)] = value;
+            }
+            return acc;
+          }, {});
+
+        // Hangup detection
+        socket.on("data", async (data) => {
+          if (data.toString().includes("HANGUP")) {
+            socket.removeAllListeners("data");
+            socket.end();
+          }
+        });
+
+        const call = new AGIChannel({
+          ...agiVariables,
+          remoteServer,
+          socket,
+        });
+        this.emit("call", call);
+
+        socket.on("end", () => {
+          call.emit("hangup");
+        });
+      }
+    });
+  }
 }
+
 module.exports = AGIServer;
